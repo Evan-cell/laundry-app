6670be3ce95e3b74e95eb4a521a1482a3d9bd318
evan <malcomchege0582@gmail.com>
2023-11-14 14:49:26 +0300

    homescreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import Homescreen from './screens/Homescreen';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Text>lAUNDRY</Text>
+      <Homescreen/>
     </SafeAreaView>
   );
 }
---
+++ screens/Homescreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React,{useEffect} from 'react'
+
+const Homescreen = () => {
+  return (
+    <View>
+      <Text>Homescreen</Text>
+    </View>
+  )
+}
+
+export default Homescreen
+
+const styles = StyleSheet.create({})
